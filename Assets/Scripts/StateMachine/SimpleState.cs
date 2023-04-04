9b9f4d02cbdbe6715ff7bb869fa2c330d8105306
ChristianTW <christian.wiggins0@gmail.com>
2023-04-04 13:19:19 -0500

    SimpleStateMachine added

---
+++ Assets/Scripts/StateMachine/SimpleState.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.events;
+
+namespace FOGPIUtilities.StateMachine
+{
+    [System.Serializable]
+    public class SimpleState
+    {
+        [Header("StateEvent")]
+        public UnityEvent StateEnter;
+        public UnityEvent StateUpdate;
+        public UnityEvent StateExit;
+
+        //[HideInInspector]
+        public virtual void OnStart()
+        {
+            StateEnter.Invoke();
+        }
+
+        public virtual void OnUpdate()
+        {
+            StateUpdate.Invoke();
+        }
+
+        public virtual void OnExit()
+        {
+            StateExit.Invoke();
+        }
+    }
+
+}
